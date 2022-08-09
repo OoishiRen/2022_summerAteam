d62d4f35b8cf672098cba393685f417378f90dd6
OoishiRen <ren-ooishi-20h@stu.kbc.ac.jp>
2022-08-09 11:06:39 +0900

    Player.cppを追加

---
+++ 2022_summerAteam/Player.cpp
@@ -0,0 +1,24 @@
+#include "Player.h"
+#include "SceneMgr.h"
+#include "Input.h"
+#include "DxLib.h"
+
+//‰Šú‰»
+void Player_Initialize() {
+
+}
+
+//I—¹ˆ—
+void Player_Finalize() {
+
+}
+
+//XV
+void Player_Update() {
+
+}
+
+//•`‰æ
+void Player_Draw() {
+
+}
---
+++ 2022_summerAteam/Player.h
@@ -0,0 +1,6 @@
+#pragma once
+
+void Player_Initialize();//‰Šú‰»
+void Player_Finalize();//I—¹ˆ—
+void Player_Update();//XV
+void Player_Draw();//•`‰æ
