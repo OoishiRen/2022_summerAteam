82fe872c70130c8e3338f3cdc42729a28f436b24
kyuya11 <83804045+kyuya11@users.noreply.github.com>
2022-08-09 15:04:06 +0900

    アカベイが簡単に追いかけてくるよ処理を作っている途中

--- 2022_summerAteam/Enemy.cpp
+++ 2022_summerAteam/Enemy.cpp
@@ -2,22 +2,73 @@
 #include "SceneMgr.h"
 #include "Enemy.h"
 
-int AkabeiImage[ENEMY_IMAGE_MAX];
+
+struct AKABEI Akabei;
+int MonsterImage[ENEMY_IMAGE_MAX];
+int AkabeiImagecount;
+
+/* 仮プレイヤー */
+int PlayerX;
+int PlayerY;
+int PlayerImage;
+float PlayerSpeed;
 
 void Enemy_Initialize() {
-	LoadDivGraph("enemy_images/monster.png", 20, 20, 1, 32, 32, AkabeiImage); //エネミー画像
+	LoadDivGraph("enemy_images/monster.png", 20, 20, 1, 16, 16, MonsterImage); //アカベイの画像
+
+	Akabei.x = 1000;
+	Akabei.y = 400;
+	Akabei.ImageCount = 0;
+	Akabei.speed = 0.0f;
+
+	PlayerImage = MonsterImage[19];
+	PlayerX = 900;
+	PlayerY = 400;
+	PlayerSpeed = 0.0f;
 }
 
 void Enemy_Finalize() {
 	for (int i = 0; i < ENEMY_IMAGE_MAX; i++) {
-		DeleteGraph(AkabeiImage[i]);
+		DeleteGraph(MonsterImage[i]);
 	}
+	DeleteGraph(PlayerImage);
 }
 
 void Enemy_Update() {
+	// アニメーション
+	if (Akabei.ImageCount == 0) {
+		Akabei.ImageCount = 1;
+
+	}
+	else {
+		Akabei.ImageCount = 0;
+	}
+
+	// 仮プレイヤーの横移動
+	if (CheckHitKey(KEY_INPUT_LEFT)) {
+		PlayerSpeed--;
+	}
+	else if (CheckHitKey(KEY_INPUT_RIGHT)) {
+		PlayerSpeed++;
+	}
+
 	// プレイヤーを追いかける処理
+	if (Akabei.x < PlayerX) {//アカベイから見て右側
+		if (Akabei.x > PlayerX) {
+			++Akabei.speed;
+		}
+	}
+	if (Akabei.x > PlayerX) {
+		if (Akabei.x < PlayerX) {//アカベイから見て左側
+			--Akabei.speed;
+		}
+	}
+
 }
 
 void Enemy_Draw() {
-	DrawRotaGraph(1000, 400, 1, 0, AkabeiImage[0], FALSE);
+
+	DrawRotaGraph(Akabei.x + Akabei.speed, Akabei.y, 1, 0, MonsterImage[Akabei.ImageCount], FALSE);
+
+	DrawRotaGraph(PlayerX + PlayerSpeed, PlayerY, 1, 0, PlayerImage, TRUE);
 }
--- 2022_summerAteam/Enemy.h
+++ 2022_summerAteam/Enemy.h
@@ -3,6 +3,16 @@
 /* ’è” */
 const int ENEMY_IMAGE_MAX = 20;
 
+
+/* \‘¢‘Ì */
+struct AKABEI {
+	int x, y;
+	float speed;
+	int ImageCount;
+};
+extern struct AKABEI Akabei;
+
+
 extern void Enemy_Initialize();
 extern void Enemy_Finalize();
 extern void Enemy_Update();
