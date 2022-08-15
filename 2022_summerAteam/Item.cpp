1d6a27590b69365e5b8d35a6776c98f777f875c8
rikutoiri <rikuto-taba-20h@stu.kbc.ac.jp>
2022-08-15 11:42:29 +0900

    アイテムの配置（仮

--- 2022_summerAteam/Item.cpp
+++ 2022_summerAteam/Item.cpp
@@ -1,14 +1,50 @@
 #include "Item.h"
 #include "Dxlib.h"
+#include "Game.h"
+
+int Dot_Handle;
+int Powerdot_Handle;
+
+int Item_Mapdata[MAP_HEIGHT][MAP_WIDTH]{
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 0},
+	{0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0},
+	{0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+	{0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0},
+	{0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+	{0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0},
+	{0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+	{0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0},
+	{0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+	{0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0},
+	{0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+	{0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0},
+	{0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+
+};
 
 void Item_Initialize() {
-
+	Dot_Handle = LoadGraph("Item_images/dot.png");
+	Powerdot_Handle = LoadGraph("Item_Images/big_dot.png");
 }
 void Item_Finalize() {
-
+	DeleteGraph(Dot_Handle);
+	DeleteGraph(Powerdot_Handle);
 }
 void Item_Update() {
-
+	for (int i = 0; i < MAP_HEIGHT; i++) {
+		for (int j = 0; j < MAP_WIDTH; j++) {
+			if (Item_Mapdata[i][j] == 1) {
+				////DrawPixel(j * MAP_SIZE+16, i * MAP_SIZE+16, GetColor(255, 255, 255));//ピクセル
+				DrawGraph(j * MAP_SIZE + 8, i * MAP_SIZE + 8, Dot_Handle, TRUE);
+			}
+			if (Item_Mapdata[i][j] == 2) {
+				////DrawPixel(j * MAP_SIZE+16, i * MAP_SIZE+16, GetColor(255, 255, 255));//ピクセル
+				DrawGraph(j * MAP_SIZE + 8, i * MAP_SIZE + 8, Powerdot_Handle, TRUE);
+			}
+		}
+	}
 }
 void Item_Draw() {
 
--- 2022_summerAteam/Item.h
+++ 2022_summerAteam/Item.h
@@ -1,5 +1,10 @@
 #pragma once
 
+
+#define MAP_SIZE 32 // スクリーンサイズから見たマップサイズを定義	//アイテム用
+#define MAP_WIDTH 19 // マップの横幅をMAP_WIDTHと定義				//アイテム用
+#define MAP_HEIGHT 15 // マップの縦幅をMAP_HEIGHTと定義				//アイテム用
+
 void Item_Initialize();
 void Item_Finalize();
 void Item_Update();
