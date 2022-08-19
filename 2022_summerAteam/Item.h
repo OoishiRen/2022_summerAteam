1886167d8001ea436ff9041c42bb97536802e312
rikutoiri <rikuto-taba-20h@stu.kbc.ac.jp>
2022-08-19 11:18:39 +0900

    パワーエサの追加（まだ食べれない） エサの配置

--- 2022_summerAteam/Item.h
+++ 2022_summerAteam/Item.h
@@ -1,11 +1,16 @@
 #pragma once
 
 
-#define MAP_SIZE 32 // スクリーンサイズから見たマップサイズを定義	//アイテム用
-#define MAP_WIDTH 19 // マップの横幅をMAP_WIDTHと定義				//アイテム用
-#define MAP_HEIGHT 15 // マップの縦幅をMAP_HEIGHTと定義				//アイテム用
+#define MAP_SIZE 16 // スクリーンサイズから見たマップサイズを定義	//アイテム用
+#define MAP_WIDTH 30 // マップの横幅をMAP_WIDTHと定義				//アイテム用
+#define MAP_HEIGHT 33 // マップの縦幅をMAP_HEIGHTと定義				//アイテム用
+
+#define DOT_IMAGE_MAX 2
+#define CNT_SPEED 15
 
 void Item_Initialize();
 void Item_Finalize();
 void Item_Update();
 void Item_Draw();
+
+void PowerdotAnim();
