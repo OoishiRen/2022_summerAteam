a29b794eec20021ed797f9cd31d535f40291789b
kazuya1031123 <kazuya-asato-20h@stu.kbc.ac.jp>
2022-09-21 15:21:18 +0900

    タイトルちょっとだけ

--- 2022_summerAteam/Menu.cpp
+++ 2022_summerAteam/Menu.cpp
@@ -8,7 +8,11 @@
 static int mTitleHandle; //画像ハンドル格納用変数
 static int score_image[15];
 int img_x = 338, img_y = 720;
-int mozi_x = 590, mozi_y = 780;
+int mozi_x = 590, mozi_y = 600;
+int mozi1_x = 615, mozi1_y = 600;
+int mozi2_x = 640, mozi2_y = 600;
+int mozi3_x = 665, mozi3_y = 600;
+int mozi4_x = 690, mozi4_y = 600;
 int num14 = 1;
 int num15 = 0;
 int num16 = 0;
@@ -28,7 +32,11 @@ void Menu_Initialize() {
 
 
 	img_x = 338, img_y = 720;//タイトルの初期位置
-	mozi_x = 590, mozi_y = 780;
+	mozi_x = 590, mozi_y = 600;
+	 mozi1_x = 615, mozi1_y = 600;
+	 mozi2_x = 640, mozi2_y = 600;
+	 mozi3_x = 665, mozi3_y = 600;
+	 mozi4_x = 690, mozi4_y = 600;
 
 }
 //終了処理
@@ -50,6 +58,36 @@ void Menu_Update() {
 	else {
 		mozi_y -= TITLE_SPD;
 	}
+	if (mozi1_y <= 50) {
+
+		mozi1_y = 50;
+	}
+	else {
+		mozi1_y -= TITLE_SPD;
+	}
+	if (mozi2_y <= 50) {
+
+		mozi2_y = 50;
+	}
+	else {
+		mozi2_y -= TITLE_SPD;
+	}
+	if (mozi3_y <= 50) {
+
+		mozi3_y = 50;
+	}
+	else {
+		mozi3_y -= TITLE_SPD;
+	}
+	if (mozi4_y <= 50) {
+
+		mozi4_y = 50;
+	}
+
+	else {
+		mozi4_y -= TITLE_SPD;
+	}
+	
 
 
 
@@ -78,8 +116,8 @@ void Menu_Draw() {
 	DrawFormatString(10, 700, 0xffffff, "Gキー、またはAボタンを押してゲーム画面へ");
 	DrawFormatString(10, 680, 0xffffff, "Sキーを押してタイトルをスキップ(ゲーム画面へ)");
 	DrawRotaGraph(mozi_x, mozi_y, 2.0f, 0, score_image[num14], TRUE, FALSE);
-	DrawRotaGraph(615, 50, 2.0f, 0, score_image[num15], TRUE, FALSE);
-	DrawRotaGraph(640, 50, 2.0f, 0, score_image[num16], TRUE, FALSE);
-	DrawRotaGraph(665, 50, 2.0f, 0, score_image[num17], TRUE, FALSE);
-	DrawRotaGraph(690, 50, 2.0f, 0, score_image[num18], TRUE, FALSE);
+	DrawRotaGraph(mozi1_x, mozi1_y, 2.0f, 0, score_image[num15], TRUE, FALSE);
+	DrawRotaGraph(mozi2_x, mozi2_y, 2.0f, 0, score_image[num16], TRUE, FALSE);
+	DrawRotaGraph(mozi3_x, mozi3_y, 2.0f, 0, score_image[num17], TRUE, FALSE);
+	DrawRotaGraph(mozi4_x, mozi4_y, 2.0f, 0, score_image[num18], TRUE, FALSE);
 }
